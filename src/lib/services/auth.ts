ffbe1346250eed5204eee60b827bae3c4a3c7d8c
Divyam <divyamkumarp@gmail.com>
2024-08-26 11:27:29 +0530

    auth logs

--- src/lib/services/auth.ts
+++ src/lib/services/auth.ts
@@ -2,6 +2,8 @@
 
 export const checkAuth = (req: Request) => {
     const API_KEY = req.headers.get("API_KEY");
+    console.log(API_KEY);
+    console.log(process.env.API_KEY);
     if (API_KEY !== process.env.API_KEY) {
         return false;
     }
